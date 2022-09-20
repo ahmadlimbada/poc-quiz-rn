f406ffd643488290e82a8ee164e12d98941dabfc
Ahmad Limbada <Ahmadlimbada@gmail.com>
2022-09-20 14:57:00 +0530

    [AL] Created basic structure of home screen

--- screens/home.js
+++ screens/home.js
@@ -1,14 +1,36 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
+import Title from '../components/title';
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Title />
+      <View style={styles.bannerContainer}>
+        <Image
+          source={{
+            uri: 'https://cdni.iconscout.com/illustration/premium/thumb/q-and-a-service-3678714-3098907.png',
+          }}
+          style={styles.banner}
+          resizeMode="contain"
+        />
+      </View>
+      <TouchableOpacity>
+        <Text>Start</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  banner: {
+    height: 300,
+    width: 300,
+  },
+  bannerContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
